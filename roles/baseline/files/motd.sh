c08749cca62da20469b125b9cc4fecc175c3c29b
tedsluis <ted.sluis@gmail.com>
2020-10-06 07:04:43 +0200

    added motd

--- roles/baseline/files/motd.sh
+++ roles/baseline/files/motd.sh
@@ -1,154 +1,52 @@
-#!/bin/bash
-# More info at https://github.com/tedsluis/motd
-# Based on https://github.com/gagle/raspberrypi-motd/blob/master/motd.sh
+USER=`whoami`
+HOSTNAME=`uname -n`
+IP=`hostname -i`
+ROOT=`df -Ph | grep -P '\s/$' | awk '{print $4}' | tr -d '\n'`
+NUM_UNIQUE_USERS=`users | tr ' ' '\n' | sort | uniq | wc -w`
+MEMORY=`free -m | grep "Mem" | awk '{print $2,"-",$3,"-",$4}'`
+SWAP=`free -m | grep "Swap" | awk '{print $2,"-",$3,"-",$4}'`
+PSA=`ps -Afl | wc -l`
+CPU=$(</sys/class/thermal/thermal_zone0/temp)
 
-function color (){
-  echo "\e[$1m$2\e[0m"
-}
-
-function extend (){
-  local str="$1"
-  let spaces=60-${#1}
-  while [ $spaces -gt 0 ]; do
-    str="$str "
-    let spaces=spaces-1
-  done
-  echo "$str"
-}
-
-function center (){
-  local str="$1"
-  let spacesLeft=(78-${#1})/2
-  let spacesRight=78-spacesLeft-${#1}
-  while [ $spacesLeft -gt 0 ]; do
-    str=" $str"
-    let spacesLeft=spacesLeft-1
-  done
-  
-  while [ $spacesRight -gt 0 ]; do
-    str="$str "
-    let spacesRight=spacesRight-1
-  done
-  
-  echo "$str"
-}
-
-function sec2time (){
-  local input=$1
-  
-  if [ $input -lt 60 ]; then
-    echo "$input seconds"
-  else
-    ((days=input/86400))
-    ((input=input%86400))
-    ((hours=input/3600))
-    ((input=input%3600))
-    ((mins=input/60))
-    
-    local daysPlural="s"
-    local hoursPlural="s"
-    local minsPlural="s"
-    
-    if [ $days -eq 1 ]; then
-      daysPlural=""
-    fi
-    
-    if [ $hours -eq 1 ]; then
-      hoursPlural=""
-    fi
-    
-    if [ $mins -eq 1 ]; then
-      minsPlural=""
-    fi
-    
-    echo "$days day$daysPlural, $hours hour$hoursPlural, $mins minute$minsPlural"
-  fi
-}
-
-borderColor=35
-headerLeafColor=32
-headerRaspberryColor=31
-greetingsColor=36
-statsLabelColor=33
-
-borderLine="━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-borderTopLine=$(color $borderColor "┏$borderLine┓")
-borderBottomLine=$(color $borderColor "┗$borderLine┛")
-borderBar=$(color $borderColor "┃")
-borderEmptyLine="$borderBar                                                                              $borderBar"
-
-# Header
-header="$borderTopLine\n$borderEmptyLine\n"
-header="$header$borderBar$(color $headerLeafColor "          .~~.   .~~.                                                         ")$borderBar\n"
-header="$header$borderBar$(color $headerLeafColor "         '. \ ' ' / .'                                                        ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "          .~ .~~~..~.                      _                          _       ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "         : .~.'~'.~. :     ___ ___ ___ ___| |_ ___ ___ ___ _ _    ___|_|      ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "        ~ (   ) (   ) ~   |  _| .'|_ -| . | . | -_|  _|  _| | |  | . | |      ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "       ( : '~'.~.'~' : )  |_| |__,|___|  _|___|___|_| |_| |_  |  |  _|_|      ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "        ~ .~ (   ) ~. ~               |_|                 |___|  |_|          ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "         (  : '~' :  )                                                        ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "          '~ .~~~. ~'                                                         ")$borderBar\n"
-header="$header$borderBar$(color $headerRaspberryColor "              '~'                                                             ")$borderBar"
-
-me=$(whoami)
-
-hostnaam=$(hostname)
-kernel=$(uname -ro)
-ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
-system="$hostnaam, ($ip), $kernel"
-
-stdinsessions=$(who -m | wc -l)
-totalsessions=$(who -l | wc -l)
-loggedinsessions=$(who -u | wc -l)
-deadsessions=$(who -d | wc -l)
-sessions="$totalsessions(total), $loggedinsessions(logins), $stdinsessions(interactive), $deadsessions (dead)"
-
-totalnumberofprocesses=$(ps -Afl | wc -l)
-numberofprocessesroot=$(ps -Afl | grep root | wc -l)
-numberofprocesses=$(ps -Afl | grep $me | wc -l)
-process="$totalnumberofprocesses(total), $numberofprocessesroot(root), $numberofprocesses(yours)"
-
-# Greetings
-greetings="$borderBar$(color $greetingsColor "$(center "Welcome back, $me!")")$borderBar\n"
-greetings="$greetings$borderBar$(color $greetingsColor "$(center "$(date +"%A, %d %B %Y, %T")")")$borderBar"
-
-# System information
-read loginFrom loginIP loginDate <<< $(last -F $me | awk 'NR==1 { print $2," (" $3 ") ",$4 " " $5 " " $6 " " $8 " / " $7 }')
-
-if [[ $loginFrom == tty ]]; then
-  read loginFrom loginIP loginDate <<< $(last -F $me | awk 'NR==1 { print $2,"local",$3 " " $4 " " $5 " " $7 " / " $6 }')
+# time of day
+HOUR=$(date +"%H")
+if [ $HOUR -lt 12  -a $HOUR -ge 0 ]
+then    TIME="morning"
+elif [ $HOUR -lt 17 -a $HOUR -ge 12 ] 
+then    TIME="afternoon"
+else
+    TIME="evening"
 fi
-
-logins="$loginFrom $loginIP $loginDate"
-
-label1="$(extend "$logins")"
-label1="$borderBar  $(color $statsLabelColor "Last Login....:") $label1$borderBar"
-
-uptime="$(sec2time $(cut -d "." -f 1 /proc/uptime))"
-uptime="$uptime ($(date -d "@"$(grep btime /proc/stat | cut -d " " -f 2) +"%d-%m-%Y %H:%M:%S"))"
-
-label2="$(extend "$uptime")"
-label2="$borderBar  $(color $statsLabelColor "Uptime........:") $label2$borderBar"
-
-label3="$(extend "$(free -m | awk 'NR==2 { printf "Total: %sMB, Used: %sMB, Free: %sMB",$2,$3,$4; }')")"
-label3="$borderBar  $(color $statsLabelColor "Memory........:") $label3$borderBar"
-
-label4="$(extend "$(df -h ~ | awk 'NR==2 { printf "Total: %sB, Used: %sB, Free: %sB",$2,$3,$4; }')")"
-label4="$borderBar  $(color $statsLabelColor "Home space....:") $label4$borderBar"
-
-label5="$(extend "$(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")ºC")"
-label5="$borderBar  $(color $statsLabelColor "Temperature...:") $label5$borderBar"
-
-label6="$(extend "$process")"
-label6="$borderBar  $(color $statsLabelColor "Processes.....:") $label6$borderBar"
-
-label7="$(extend "$sessions")"
-label7="$borderBar  $(color $statsLabelColor "Sessions......:") $label7$borderBar"
-
-label8="$(extend "$system")"
-label8="$borderBar  $(color $statsLabelColor "System........:") $label8$borderBar"
-
-stats="$label8\n$label1\n$label7\n$label2\n$label6\n$label3\n$label4\n$label5"
-
-# Print motd
-echo -e "$header\n$borderEmptyLine\n$greetings\n$borderEmptyLine\n$stats\n$borderEmptyLine\n$borderBottomLine"
+ 
+#System uptime
+uptime=`cat /proc/uptime | cut -f1 -d.`
+upDays=`expr $uptime / 60 / 60 / 24`
+upHours=`expr $uptime / 60 / 60 % 24`
+upMins=`expr $uptime / 60 % 60`
+upSecs=`expr $uptime % 60`
+ 
+#System load
+LOAD1=`cat /proc/loadavg | awk {'print $1'}`
+LOAD5=`cat /proc/loadavg | awk {'print $2'}`
+LOAD15=`cat /proc/loadavg | awk {'print $3'}`
+ 
+COLOR_COLUMN="\e[1m-"
+COLOR_VALUE="\e[31m"
+RESET_COLORS="\e[0m"
+ 
+echo -e " 
+\e[7m--- GOOD $TIME $USER ----\e[0m  $RESET_COLORS 
+ 
+ $COLOR_COLUMN- Hostname$RESET_COLORS............: $COLOR_VALUE $HOSTNAME $RESET_COLORS
+ $COLOR_COLUMN- IP Address$RESET_COLORS..........: $COLOR_VALUE $IP $RESET_COLORS
+ $COLOR_COLUMN- Release$RESET_COLORS.............: $COLOR_VALUE `cat /etc/redhat-release` $RESET_COLORS
+ $COLOR_COLUMN- Users$RESET_COLORS...............: $COLOR_VALUE Currently $NUM_UNIQUE_USERS DIFFERENT user(s) logged on $RESET_COLORS
+ $COLOR_COLUMN- Current user$RESET_COLORS........: $COLOR_VALUE $USER $RESET_COLORS
+ $COLOR_COLUMN- CPU usage$RESET_COLORS...........: $COLOR_VALUE $LOAD1 - $LOAD5 - $LOAD15 (1-5-15 min) $RESET_COLORS
+ $COLOR_COLUMN- Cores Temperature$RESET_COLORS...: $COLOR_VALUE $((CPU/1000))'C $RESET_COLORS
+ $COLOR_COLUMN- Memory used$RESET_COLORS.........: $COLOR_VALUE $MEMORY (total-free-used) $RESET_COLORS
+ $COLOR_COLUMN- Swap in use$RESET_COLORS.........: $COLOR_VALUE $SWAP (total-used-free) MB $RESET_COLORS
+ $COLOR_COLUMN- Processes$RESET_COLORS...........: $COLOR_VALUE $PSA running $RESET_COLORS
+ $COLOR_COLUMN- System uptime$RESET_COLORS.......: $COLOR_VALUE $upDays days $upHours hours $upMins minutes $upSecs seconds $RESET_COLORS
+ $COLOR_COLUMN- Disk space HOME$RESET_COLORS.....: $COLOR_VALUE $ROOT remaining $RESET_COLORS
+" 
